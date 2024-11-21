9db29be948b8714083e8f70369c6b81ba55c82ad
andreamsgi27 <andreamsgi27@hotmail.com>
2024-11-21 22:36:59 +0100

    estructura de mockito

--- src/test/java/com/factoriaf5/rps/application/GameTest.java
+++ src/test/java/com/factoriaf5/rps/application/GameTest.java
@@ -1,15 +1,31 @@
 package com.factoriaf5.rps.application;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.junit.jupiter.api.BeforeEach;
 import com.factoriaf5.rps.models.Form;
 
 public class GameTest {
-    
+
+    @Mock
+    private Form mockPlayerForm;
+
+    private Game game;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this); // Inicializa los mocks
+        game = new Game(mockPlayerForm);
+    }
+
     @Test
-    public void justASimpleTest() {
+    public void play() {
+        
         
 
     }
