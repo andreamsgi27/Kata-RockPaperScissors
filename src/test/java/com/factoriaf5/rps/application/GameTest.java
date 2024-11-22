337873e336e7f969eeac8c0f40d07c24eb410844
andreamsgi27 <andreamsgi27@hotmail.com>
2024-11-22 01:18:25 +0100

    "Added new test cases for Lizard and Spock forms in GameTest.java"

--- src/test/java/com/factoriaf5/rps/application/GameTest.java
+++ src/test/java/com/factoriaf5/rps/application/GameTest.java
@@ -5,9 +5,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import com.factoriaf5.rps.models.Form;
+import com.factoriaf5.rps.models.Lizard;
+import com.factoriaf5.rps.models.Paper;
 import com.factoriaf5.rps.models.Rock;
 import com.factoriaf5.rps.models.Scissors;
-import com.factoriaf5.rps.models.Paper;
+import com.factoriaf5.rps.models.Spock;
 
 public class GameTest {
 
@@ -32,6 +34,22 @@ public class GameTest {
         Form formPlayer = new Paper();
         Game game = new Game(formPlayer);
         game.guessChoice(3);
+        String result = game.play();
+        assertEquals("Empate", result);
+    }
+    @Test
+    public void playReturnsTieLizard() {
+        Form formPlayer = new Lizard();
+        Game game = new Game(formPlayer);
+        game.guessChoice(4);
+        String result = game.play();
+        assertEquals("Empate", result);
+    }
+    @Test
+    public void playReturnsTieSpock() {
+        Form formPlayer = new Spock();
+        Game game = new Game(formPlayer);
+        game.guessChoice(5);
         String result = game.play();
         assertEquals("Empate", result);
     }
@@ -84,6 +102,70 @@ public class GameTest {
         game.guessChoice(2);
         String result = game.play();
         assertEquals("Gana computadora", result);
+    }
+    @Test
+    public void playReturnsPCWinsLizardVsScissors() {
+        Form formPlayer = new Lizard();
+        Game game = new Game(formPlayer);
+        game.guessChoice(2);
+        String result = game.play();
+        assertEquals("Gana computadora", result);
+    }
+    @Test
+    public void playReturnsPCWinsLizardVsRock() {
+        Form formPlayer = new Lizard();
+        Game game = new Game(formPlayer);
+        game.guessChoice(1);
+        String result = game.play();
+        assertEquals("Gana computadora", result);
+    }
+    @Test
+    public void playReturnsPCWinsSpockVsLizard() {
+        Form formPlayer = new Spock();
+        Game game = new Game(formPlayer);
+        game.guessChoice(4);
+        String result = game.play();
+        assertEquals("Gana computadora", result);
+    }
+    @Test
+    public void playReturnsPCWinsSpockVsPaper() {
+        Form formPlayer = new Spock();
+        Game game = new Game(formPlayer);
+        game.guessChoice(3);
+        String result = game.play();
+        assertEquals("Gana computadora", result);
+    }
+    @Test
+    public void playReturnsPlayerWinLizardVsPaper() {
+        Form formPlayer = new Lizard();
+        Game game = new Game(formPlayer);
+        game.guessChoice(3);
+        String result = game.play();
+        assertEquals("Gana jugador", result);
+    }
+    @Test
+    public void playReturnsPlayerWinLizardVsSpock() {
+        Form formPlayer = new Lizard();
+        Game game = new Game(formPlayer);
+        game.guessChoice(5);
+        String result = game.play();
+        assertEquals("Gana jugador", result);
+    }
+    @Test
+    public void playReturnsPlayerWinSpockVsScissors() {
+        Form formPlayer = new Spock();
+        Game game = new Game(formPlayer);
+        game.guessChoice(2);
+        String result = game.play();
+        assertEquals("Gana jugador", result);
+    }
+    @Test
+    public void playReturnsPlayerWinSpockVsRock() {
+        Form formPlayer = new Spock();
+        Game game = new Game(formPlayer);
+        game.guessChoice(1);
+        String result = game.play();
+        assertEquals("Gana jugador", result);
     }
 
 
